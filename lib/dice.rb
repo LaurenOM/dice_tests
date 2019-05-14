c725feb992ab58c4c21ad79853a0abf8d1ba15f7
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-14 11:52:28 +0100

    working test for multiple rolls

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,6 +1,32 @@
-
 class Dice 
+	attr_reader :number_of_rolls
+	
+	def initialize 
+		@number_of_rolls = 3
+	end
+
   def show_number(number)
-    return number
-  end
+		return random_number
+	end
+	
+	def roll_dice
+		puts "roll dice?"
+		move = gets.chomp
+	end
+
+	def random_number
+		rand(1..6)
+	end
+
+	def multiple_rolls
+		numbers = []
+		rolls = @number_of_rolls.to_i
+		
+		rolls.times do
+			numbers << random_number
+		end
+
+		numbers
+	end
 end
+
--- spec/dice_test.rb
+++ spec/dice_test.rb
@@ -1,31 +1,46 @@
 require "dice"
 
-# describe Dice do 
-#   it 'can roll a dice' do
+describe Dice do 
+  it 'can roll a dice' do
+    dice = Dice.new
+    
+    move = dice.roll_dice
 
-#    end
-# end
+    expect(move).not_to eq(nil)
+  end
+end
 
 describe Dice do 
   it 'can show a number between 1 and 6' do 
-  # Arrange
     dice = Dice.new
     number = 5 
   
-    # Act 
     dice.show_number(number)
-    # Assert 
+     
     expect(number).not_to eq(nil)
   end
 end
 
-# describe Dice do 
-#   it 'can randomly pick a number' do 
-#   end
-# end
+describe Dice do 
+  it 'can randomly pick a number between 1 and 6' do
+    dice = Dice.new
+    range = (1..6)
 
-# describe Dice do 
-#     it 'can roll any given number of dice at one time' do 
-#     end
-# end
+    number = dice.random_number
+
+    expect(range).to include(number)
+  end
+end
+
+describe Dice do 
+  it 'can roll any given number of dice at one time' do 
+    dice = Dice.new 
+
+    output = dice.multiple_rolls
+    output_length = output.length
+
+    expect(output_length).to eq(dice.number_of_rolls)
+  end
+end
+
 
